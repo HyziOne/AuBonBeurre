e7519d9015100a35af8dfe3c0897044becb1e7dd
HyziOne <72913682+HyziOne@users.noreply.github.com>
2021-09-14 14:08:55 +0200

    Add files via upload

---
+++ random values.py
@@ -0,0 +1,29 @@
+import random
+from random import *
+import math
+
+temps_cuve = round(uniform(2.5,4),1)
+temps_ext = round(uniform(8,14),1)
+pds_lait = randint(3512,4607)
+
+ph = round(uniform(6.8,7.2),1)
+k_plus = randint(35,47)
+conc_nacl = round(uniform(1,1.7),1)
+niv_salmonelle = randint(17,37)
+niv_ecoli = randint(35,49)
+niv_listeria = randint(28,54)
+
+
+
+
+print(temps_cuve)
+print(temps_ext)
+print(pds_lait)
+#print(pds_fini)
+print(ph)
+print(k_plus)
+print(conc_nacl)
+print(niv_salmonelle)
+print(niv_ecoli)
+print(niv_listeria)
+
